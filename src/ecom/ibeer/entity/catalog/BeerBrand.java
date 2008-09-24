dc5d553f74e7c22fc048ad730d19e215c4528296
ariel.maor <ariel.maor@a46959c4-2456-0410-986f-2dcce5375f4c>
2008-09-24 22:08:12 +0000

    [REL] 1. Remove unnecessary list methods. 2. Spelling corrections. 3. Alignments.

--- src/ecom/ibeer/entity/catalog/BeerBrand.java
+++ src/ecom/ibeer/entity/catalog/BeerBrand.java
@@ -9,16 +9,14 @@ import java.util.Iterator;
 public class BeerBrand implements Serializable {
 
 	/**
- *
- */
-private Collection<Beer> beers;
+	 *
+	 */
+	private Collection<Beer> beers;
 
-	
-	
 	/*
- * (non-javadoc)
- */
-private String name;
+	 * (non-javadoc)
+	 */
+	private String name;
 
 	/**
 	 * Getter of the property <tt>beers</tt>
@@ -26,156 +24,39 @@ private String name;
 	 * @return Returns the beers.
 	 * 
 	 */
-	
-	public Collection<Beer> getBeers()
-	{
+
+	public Collection<Beer> getBeers() {
 		return beers;
 	}
 
-	
-	
-	 
-	/**
-	 * Returns an iterator over the elements in this collection. 
-	 *
-	 * @return an <tt>Iterator</tt> over the elements in this collection
-	 * @see	java.util.Collection#iterator()
-	 * 
-	 */
-	public Iterator<Beer> beersIterator(){
-		return beers.iterator();
-	}
-	
-	/**
-	 * Returns <tt>true</tt> if this collection contains no elements.
-	 *
-	 * @return <tt>true</tt> if this collection contains no elements
-	 * @see	java.util.Collection#isEmpty()
-	 *
-	 */
-	public boolean isBeersEmpty(){
-		return beers.isEmpty();
-	}
-	
-	/**
-	 * Returns <tt>true</tt> if this collection contains the specified element. 
-	 *
-	 * @param element whose presence in this collection is to be tested.
-	 * @see	java.util.Collection#contains(Object)
-	 *
-	 */
-	public boolean containsBeers(Beer beers){
-		return this.beers.contains(beers);
-	}
-	
-	/**
-	 * Returns <tt>true</tt> if this collection contains all of the elements
-	 * in the specified collection.
-	 *
-	 * @param elements collection to be checked for containment in this collection.
-	 * @see	java.util.Collection#containsAll(Collection)
-	 *
-	 */
-	public boolean containsAllBeers(Collection<Beer> beers){
-		return this.beers.containsAll(beers);
-	}
-	
-	/**
-	 * Returns the number of elements in this collection.
-	 *
-	 * @return the number of elements in this collection
-	 * @see	java.util.Collection#size()
-	 *
-	 */
-	public int beersSize(){
-		return beers.size();
-	}
-	
-	/**
-	 * Returns all elements of this collection in an array.
-	 *
-	 * @return an array containing all of the elements in this collection
-	 * @see	java.util.Collection#toArray()
-	 *
-	 */
-	public Beer[] beersToArray(){
-		return beers.toArray(new Beer[beers.size()]);
-	}
-	 
 	/**
 	 * Setter of the property <tt>beers</tt>
 	 *
 	 * @param beers the beers to set.
 	 *
 	 */
-	public void setBeers(Collection<Beer> beers){
+	public void setBeers(Collection<Beer> beers) {
 		this.beers = beers;
 	}
-	
-	
-	/**
-	 * Ensures that this collection contains the specified element (optional
-	 * operation). 
-	 *
-	 * @param element whose presence in this collection is to be ensured.
-	 * @see	java.util.Collection#add(Object)
-	 *
-	 */
-	public boolean addBeers(Beer beers){
-		return this.beers.add(beers);
-	}
-	
-	/**
-	 * Removes a single instance of the specified element from this
-	 * collection, if it is present (optional operation).
-	 *
-	 * @param element to be removed from this collection, if present.
-	 * @see	java.util.Collection#add(Object)
-	 *
-	 */
-	public boolean removeBeers(Beer beers){
-		return this.beers.remove(beers);
-	}
-	
-	/**
-	 * Removes all of the elements from this collection (optional operation).
-	 *
-	 * @see	java.util.Collection#clear()
-	 *
-	 */
-	public void clearBeers(){
-		this.beers.clear();
-	}
 
 
-
-
-	
-	 
 	/**
 	 * Getter of the property <tt>name</tt>
 	 *
 	 * @return Returns the name.
 	 * 
 	 */
-	
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-
-
-
-	
-	
 	/**
 	 * Setter of the property <tt>name</tt>
 	 *
 	 * @param name The name to set.
 	 *
 	 */
-	public void setName(String name ){
+	public void setName(String name) {
 		this.name = name;
 	}
 
--- src/ecom/ibeer/entity/order/OrderLine.java
+++ src/ecom/ibeer/entity/order/OrderLine.java
@@ -5,190 +5,121 @@ import java.io.Serializable;
 
 /** 
  */
-public class OrderLine implements Serializable{
+public class OrderLine implements Serializable {
 
-	/*
- * (non-javadoc)
- */
-private int quantity;
+	private int quantity;
 
-	
-	 
-	/*
- * (non-javadoc)
- */
-private double priceAtBuy;
+	private double priceAtBuy;
 
+	private Product product = null;
 
+	private Order order = null;
 
-	/*
- * (non-javadoc)
- */
-private Product product = null;
-
-
-
-
-
-	/*
- * (non-javadoc)
- */
-private Order order = null;
-
-
-
-
-
-	/*
- * (non-javadoc)
- */
-private int state;
-
-
-
-
+	private int state;
 
 	/**
 	 * Getter of the property <tt>quantity</tt>
-	 *
+	 * 
 	 * @return Returns the quantity.
 	 * 
 	 */
-	
-	public int getQuantity()
-	{
+	public int getQuantity() {
 		return quantity;
 	}
 
-	
-	
 	/**
 	 * Setter of the property <tt>quantity</tt>
-	 *
-	 * @param quantity The quantity to set.
-	 *
+	 * 
+	 * @param quantity
+	 *            The quantity to set.
+	 * 
 	 */
-	public void setQuantity(int quantity ){
+	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
 
-
-
-	
-	 
 	/**
 	 * Getter of the property <tt>priceAtBuy</tt>
-	 *
+	 * 
 	 * @return Returns the priceAtBuy.
 	 * 
 	 */
-	
-	public double getPriceAtBuy()
-	{
+	public double getPriceAtBuy() {
 		return priceAtBuy;
 	}
 
-
-
-	
-	
 	/**
 	 * Setter of the property <tt>priceAtBuy</tt>
-	 *
-	 * @param priceAtBuy The priceAtBuy to set.
-	 *
+	 * 
+	 * @param priceAtBuy
+	 *            The priceAtBuy to set.
+	 * 
 	 */
-	public void setPriceAtBuy(double priceAtBuy ){
+	public void setPriceAtBuy(double priceAtBuy) {
 		this.priceAtBuy = priceAtBuy;
 	}
 
-
-
-	
-	 
 	/**
 	 * Getter of the property <tt>product</tt>
-	 *
+	 * 
 	 * @return Returns the product.
 	 * 
 	 */
-	
-	public Product getProduct()
-	{
+	public Product getProduct() {
 		return product;
 	}
 
-
-
-	
-	
 	/**
 	 * Setter of the property <tt>product</tt>
-	 *
-	 * @param product The product to set.
-	 *
+	 * 
+	 * @param product
+	 *            The product to set.
+	 * 
 	 */
-	public void setProduct(Product product ){
+	public void setProduct(Product product) {
 		this.product = product;
 	}
 
-
-
-	
-	 
 	/**
 	 * Getter of the property <tt>order</tt>
-	 *
+	 * 
 	 * @return Returns the order.
 	 * 
 	 */
-	
-	public Order getOrder()
-	{
+
+	public Order getOrder() {
 		return order;
 	}
 
-
-
-	
-	
 	/**
 	 * Setter of the property <tt>order</tt>
-	 *
-	 * @param order The order to set.
-	 *
+	 * 
+	 * @param order
+	 *            The order to set.
+	 * 
 	 */
-	public void setOrder(Order order ){
+	public void setOrder(Order order) {
 		this.order = order;
 	}
 
-
-
-	
-	 
 	/**
 	 * Getter of the property <tt>state</tt>
-	 *
+	 * 
 	 * @return Returns the state.
 	 * 
 	 */
-	
-	public int getState()
-	{
+	public int getState() {
 		return state;
 	}
 
-
-
-	
-	
 	/**
 	 * Setter of the property <tt>state</tt>
-	 *
-	 * @param state The state to set.
-	 *
+	 * 
+	 * @param state
+	 *            The state to set.
+	 * 
 	 */
-	public void setState(int state ){
+	public void setState(int state) {
 		this.state = state;
 	}
 
